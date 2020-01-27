d8231202e181dcd7dd3870afbfb06a4a5cd89edd
Evan Jeffries <ejeffries14@gmail.com>
2020-01-27 08:47:36 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,30 @@
 class Waiter
+  attr_accessor :name, :years
 
+  @@all = []
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select do |m|
+      m.waiter == self
+    end
+  end
+
+  def best_tipper
+    Customer.all.each do |amount|
+      amount.tip.max == self
+    end
+  end
 end
